79494f6f93104a52abbef1b7d83b2039314df3b8
MrTICKTAKEN <ticktaken25@gmail.com>
2024-02-14 11:10:29 -0600

    Agregar archivos de proyecto.

---
+++ ParOImpar/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ParOImpar
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Ingresa un número");
+            int numero = Convert.ToInt32(Console.ReadLine());
+
+            if (numero % 2 == 0)
+            {
+                Console.WriteLine("El numero es par");
+            }
+            else
+            {
+                Console.WriteLine("El numero es impar");
+            }
+        }
+    }
+}
